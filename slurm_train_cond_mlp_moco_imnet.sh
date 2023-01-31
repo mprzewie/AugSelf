418e1b97898ef39f028eacde0a14bbef49429da2
Marcin Przewięźlikowski <m.przewie@gmail.com>
2023-01-31 15:18:07 +0100

    work on grouping

--- slurm_train_cond_mlp_moco_imnet.sh
+++ slurm_train_cond_mlp_moco_imnet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=AugSelf_MOCO
 #SBATCH --qos=big
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --mem=32G
 #SBATCH --partition=dgxmatinf,dgxa100
 #SBATCH --exclude=szerszen
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=32
 #SBATCH --ntasks=1
 
 set -e
@@ -25,7 +25,8 @@ FRAMEWORK=moco
 BACKBONE=resnet50
 PRETRAIN_DATASET="imagenet100"
 MAX_EPOCHS=500
-
+FREQ=50
+EVAL_FREQ=50
 
 BASE_DIR="/shared/results/przewiez/uj/AugSelf"
 
@@ -35,14 +36,14 @@ source setup_datadirs.sh
 
 for AUG_TREATMENT in "mlp";
 do
-  for AUG_NN_DEPTH in 3 2;
+  for AUG_NN_DEPTH in 2 3;
   do
-    for AUG_NN_WIDTH in 256;
+    for AUG_NN_WIDTH in 16;
     do
       for AUG_INJ in "proj-cat"
       do
         {
-          EXTRA_ARGS="--aug-cond crop color --base-lr 0.05 --wd 1e-4 --ckpt-freq 50 --eval-freq 50 --num-workers 16 --distributed"
+          EXTRA_ARGS="--aug-cond crop color --base-lr 0.05 --wd 1e-4 --ckpt-freq ${FREQ} --eval-freq ${FREQ} --num-workers 16 --distributed"
           SUFFIX="crop_color"
           source single_experiment.sh
         }
