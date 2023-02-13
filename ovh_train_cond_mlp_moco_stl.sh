082caeb2afd69d4105e0817c03848dcbce9c6d23
Mateusz Pyla <mateuszpyla@gmail.com>
2023-02-13 13:23:51 +0000

    scripts moco+cond stl10/imnet100

--- ovh_train_cond_mlp_moco_stl.sh
+++ ovh_train_cond_mlp_moco_stl.sh
@@ -11,18 +11,17 @@ SRC_DIR="/home/mpyl/contrastif/AugSelf/"
 RES_DIR="/storage/shared/results/mateusz.pyla/AugSelf/"
 
 FRAMEWORK=moco
-BACKBONE=resnet50
-PRETRAIN_DATASET=imagenet100
-# IN100_PATH="/storage/shared/datasets/ImageNet100_ssl"
-MAX_EPOCHS=300
-FREQ=50
-EVAL_FREQ=50
+BACKBONE=resnet18
+PRETRAIN_DATASET=stl10
+MAX_EPOCHS=200
+FREQ=20
+EVAL_FREQ=20
 SEED=1997
 
 EXP_NAME="${FRAMEWORK}-${BACKBONE}-${PRETRAIN_DATASET}_cond"
 OUT_DIR="${RES_DIR}${EXP_NAME}"
 
-CUDA="0,1,2,3"
+CUDA="2"
 #nvidia-smi
 
 cd "${SRC_DIR}"
@@ -39,8 +38,8 @@ do
       for AUG_INJ in "proj-cat"
       do
         {
-          EXTRA_ARGS="--aug-cond crop color --base-lr 0.05 --wd 1e-4 --ckpt-freq ${FREQ} --eval-freq ${FREQ} --num-workers 16 --distributed"
-          SUFFIX="crop_color_demo"
+          EXTRA_ARGS="--aug-cond crop color --base-lr 0.05 --wd 1e-4 --ckpt-freq ${FREQ} --eval-freq ${FREQ} --num-workers 16"
+          SUFFIX="crop_color"
           source "${SRC_DIR}single_experiment.sh"
         }
       done
