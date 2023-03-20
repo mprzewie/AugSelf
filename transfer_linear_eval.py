7cafc0125f5129d922c38236b5bd1d87ca0d2e17
Marcin Przewięźlikowski <m.przewie@gmail.com>
2023-03-20 10:13:28 +0100

    towards regression and detection evaluation

--- transfer_linear_eval.py
+++ transfer_linear_eval.py
@@ -3,6 +3,7 @@ from functools import partial
 from copy import deepcopy
 from pathlib import Path
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,23 +19,78 @@ from trainers import collect_features
 from utils import Logger, get_engine_mock
 
 
-def build_step(X, Y, classifier, optimizer, w):
+def build_step(X, Y, classifier, optimizer, w, criterion_fn):
     def step():
         optimizer.zero_grad()
-        loss = F.cross_entropy(classifier(X), Y, reduction='sum')
+        loss = criterion_fn(classifier(X), Y, reduction='sum')
         for p in classifier.parameters():
             loss = loss + p.pow(2).sum().mul(w)
         loss.backward()
         return loss
     return step
 
+def l1_criterion_fn(normalize_input: bool=False, normalize_target: bool=False):
+    def fn(input, target, **kwargs):
+        if normalize_input:
+            input = nn.functional.normalize(input, dim=1)
+        if normalize_target:
+            target = nn.functional.normalize(target, dim=1)
 
-def compute_accuracy(X, Y, classifier, metric):
+        return F.l1_loss(input, target, **kwargs)
+
+    return fn
+
+def r2_fn(normalize_input: bool=False, normalize_target: bool=False):
+
+    def r2_score(y, x):
+        """https://github.com/ruchikachavhan/amortized-invariance-learning-ssl/blob/f832c17ce3d59c7a16cfba3caeac4438034cab23/r2score.py#L4"""
+        if normalize_input:
+            x = nn.functional.normalize(x, dim=1)
+        if normalize_target:
+            y = nn.functional.normalize(y, dim=1)
+
+        print("r2 pre reshape", x.shape, y.shape)
+        x = x.flatten().detach().cpu().numpy()
+        y = y.flatten().detach().cpu().numpy()
+
+
+        A = np.vstack([x, np.ones(len(x))]).T
+
+        print("r2: x, y, A", x.shape, y.shape, A.shape)
+        # Use numpy's least squares function
+        m, c = np.linalg.lstsq(A, y)[0]
+
+        # print(m, c)
+        # 1.97 -0.11
+
+        # Define the values of our least squares fit
+        f = m * x + c
+
+        # print(f)
+        # [ 1.86  3.83  5.8   7.77  9.74]
+
+        # Calculate R^2 explicitly
+        yminusf2 = (y - f)**2
+        sserr = sum(yminusf2)
+        mean = float(sum(y)) / float(len(y))
+        yminusmean2 = (y - mean)**2
+        sstot = sum(yminusmean2)
+        R2 = 1. -(sserr / sstot)
+        return R2
+
+    return r2_score
+
+
+def compute_accuracy(X, Y, classifier, metric_name_or_fn):
     with torch.no_grad():
-        preds = classifier(X).argmax(1)
-        if metric == 'top1':
+
+        preds = classifier(X)
+        if metric_name_or_fn in ["top1", "class-avg"]:
+            preds = preds.argmax(1)
+
+        if metric_name_or_fn == 'top1':
             acc = (preds == Y).float().mean().item()
-        elif metric == 'class-avg':
+        elif metric_name_or_fn == 'class-avg':
             total, count = 0., 0.
             for y in range(0, Y.max().item()+1):
                 masks = Y == y
@@ -42,8 +98,13 @@ def compute_accuracy(X, Y, classifier, metric):
                     total += (preds[masks] == y).float().mean().item()
                     count += 1
             acc = total / count
+
         else:
-            raise Exception(f'Unknown metric: {metric}')
+            assert not isinstance(metric_name_or_fn, str)
+            acc = metric_name_or_fn(Y, preds)
+
+        # else:
+        #     raise Exception(f'Unknown metric: {metric_name_or_fn}')
     return acc
 
 
@@ -60,7 +121,6 @@ def main(local_rank, args):
     logger = Logger(
         logdir=logdir, resume=True, wandb_suffix=f"lin-{args.dataset}", args=args,
         job_type="eval_linear"
-
     )
 
     # DATASETS
@@ -75,7 +135,25 @@ def main(local_rank, args):
     trainloader = build_dataloader(datasets['train'], drop_last=False)
     valloader   = build_dataloader(datasets['val'],   drop_last=False)
     testloader  = build_dataloader(datasets['test'],  drop_last=False)
+    trainvalloader = build_dataloader(datasets["trainval"], drop_last=False)
+
     num_classes = datasets['num_classes']
+
+    if args.metric in ["top1", 'class-avg']:
+        criterion_fn = F.cross_entropy
+        metric = args.metric
+    # elif args.dataset == "celeba":
+    #     criterion_fn = l1_criterion_fn(normalize_target=True)
+    #     metric = r2_fn(normalize_target=True)
+    # elif args.dataset == "lspose":
+    #     criterion_fn = l1_criterion_fn(normalize_target=True, normalize_input=True)
+    #     metric = r2_fn(normalize_target=True, normalize_input=True)
+    # elif args.dataset == "300w":
+    elif args.metric == "r2":
+        criterion_fn = l1_criterion_fn()
+        metric = r2_fn()
+    else:
+        raise NotImplementedError((args.dataset, args.metric))
 
     for ckpt_path in sorted(args.ckpt):
         engine_mock = get_engine_mock(ckpt_path=ckpt_path)
@@ -93,6 +171,13 @@ def main(local_rank, args):
         X_train, Y_train = collect_features(backbone, trainloader, device, normalize=False)
         X_val,   Y_val   = collect_features(backbone, valloader,   device, normalize=False)
         X_test,  Y_test  = collect_features(backbone, testloader,  device, normalize=False)
+        X_trainval, Y_trainval  = collect_features(backbone, trainvalloader,  device, normalize=False)
+
+        print(f"{X_train.shape=}, {Y_train.shape=}")
+        print(f"{X_val.shape=}, {Y_val.shape=}")
+        print(f"{X_test.shape=}, {Y_test.shape=}")
+        print(f"{X_trainval.shape=}, {Y_trainval.shape=}")
+
         classifier = nn.Linear(args.num_backbone_features, num_classes).to(device)
         optim_kwargs = {
             'line_search_fn': 'strong_wolfe',
@@ -108,8 +193,10 @@ def main(local_rank, args):
         best_classifier = None
         for w in torch.logspace(-6, 5, steps=45).tolist():
             optimizer = optim.LBFGS(classifier.parameters(), **optim_kwargs)
-            optimizer.step(build_step(X_train, Y_train, classifier, optimizer, w))
-            acc = compute_accuracy(X_val, Y_val, classifier, args.metric)
+
+            optimizer.step(
+                build_step(X_train, Y_train, classifier, optimizer, w, criterion_fn=criterion_fn))
+            acc = compute_accuracy(X_val, Y_val, classifier, metric)
 
             if best_acc < acc:
                 best_acc = acc
@@ -132,11 +219,11 @@ def main(local_rank, args):
 
         logger.log_msg(f'BEST: w={best_w:.4e}, acc={best_acc:.4f}')
 
-        X = torch.cat([X_train, X_val], 0)
-        Y = torch.cat([Y_train, Y_val], 0)
+        # X = torch.cat([X_train, X_val], 0)
+        # Y = torch.cat([Y_train, Y_val], 0)
         optimizer = optim.LBFGS(best_classifier.parameters(), **optim_kwargs)
-        optimizer.step(build_step(X, Y, best_classifier, optimizer, best_w))
-        acc = compute_accuracy(X_test, Y_test, best_classifier, args.metric)
+        optimizer.step(build_step(X_trainval, Y_trainval, best_classifier, optimizer, best_w, criterion_fn=criterion_fn))
+        acc = compute_accuracy(X_test, Y_test, best_classifier, metric_name_or_fn=metric)
         logger.log_msg(f'test acc={acc:.4f}')
         logger.log(
             engine=engine_mock, global_step=-1,
@@ -156,7 +243,7 @@ if __name__ == '__main__':
     parser.add_argument('--model', type=str, default='resnet18')
     parser.add_argument('--print-freq', type=int, default=10)
     parser.add_argument('--distributed', action='store_true')
-    parser.add_argument('--metric', type=str, default='top1', choices=["top1", 'class-avg'])
+    parser.add_argument('--metric', type=str, default='top1', choices=["top1", 'class-avg', "r2"])
     args = parser.parse_args()
     args.backend = 'nccl' if args.distributed else None
     args.num_backbone_features = 512 if args.model.endswith('resnet18') else 2048
